0a3721c168be06f4aa47d378b18937c4fc18dba3
chomow <dlaplagne16@gmail.com>
2025-11-26 10:02:32 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+// Service Worker pour l'application Nutrition Cancer
+const CACHE_NAME = 'nutrition-cancer-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - retourner la réponse du cache
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
